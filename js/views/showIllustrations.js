c3c194b9ed15833e35539359e61ec0b055fa411c
Tova Jertfelt <tovizitan@gmail.com>
2022-08-23 10:11:21 +0200

    första galleriet klart

--- js/views/showIllustrations.js
+++ js/views/showIllustrations.js
@@ -1,36 +1,72 @@
 //  console.log("showIllustrations.js is connected")
 
-const illustrationGallery = document.getElementById("ownIllustrationGallery");
+const illustrationGallery = document.getElementById("galleryOwnIllustration");
+const slideDivIllustrationGallery = document.getElementById("slidesIllustrationsOwn")
+
+let firstColumn = document.getElementById("slidesFirstCol");
+let scndColumn = document.getElementById("slides2ndCol");
 
 let illustrationsArray = [];
 
 //Create:
 const drawGallery = (data) => {
-  data.forEach(item => {
-    let slides = document.createElement("div");
-    slides.classList.add("slides");
+  // console.log(data);
+  let even = data.filter(function(element, index, array) {
+    return (index %2 ===0)
+  })
+  let odd = data.filter(function(element, index, array) {
+    return (index %2 !==0)
+  })
 
-    slides.innerHTML=`
-    <div id="${item.fields.title}">
+
+  
+  even.forEach(item => {
+    let slidesDiv = document.createElement("div");
+  
+   
+    slidesDiv.innerHTML=`
+    <div class="pointer"
+    id="${item.fields.title}">
     <a href="">
     <img src="${item.fields.image.fields.file.url}"
     alt="${item.fields.title} ${item.fields.description}">
+    <span class="gallery__description">
+    <h3 class="text--m headerfont">${item.fields.title}</h3>
+    <h4 class="text--s">${item.fields.description}</h4>
+    </span>
     </a>
     </div>
     `
-
-   illustrationGallery.appendChild(slides);
-    
+    scndColumn.appendChild(slidesDiv);
   })
 
+  odd.forEach(item => {
+    let slidesDiv = document.createElement("div");
+   
+    slidesDiv.innerHTML=`
+    <div class="pointer"
+    id="${item.fields.title}">
+    <a href="">
+    <img src="${item.fields.image.fields.file.url}"
+    alt="${item.fields.title} ${item.fields.description}">
+    <span class="gallery__description">
+    <h3 class="text--m headerfont">${item.fields.title}</h3>
+    <h4 class="text--s">${item.fields.description}</h4>
+    </span>
+    </a>
+    </div>
+    `
+    firstColumn.appendChild(slidesDiv);
+  })
  
   
 }
 
+
+
 async function getIllustrations() {
   try{
   let res = await fetch("../js/data/ownIllustrations.json");
-  // const resOff = await fetch("https://jertfelt.github.io/js/data/categories.json");
 
   const data= await res.json();
 
@@ -48,36 +84,3 @@ console.log(error)}
 
 getIllustrations();
 
-
-const slideGallery = document.querySelector('.slides');
-const slides = slideGallery.querySelectorAll('div');
-const thumbnailContainer = document.querySelector('.thumbnails');
-const slideCount = slides.length;
-const slideWidth = 540;
-
-const highlightThumbnail = () => {
-  thumbnailContainer
-    .querySelectorAll('div.highlighted')
-    .forEach(el => el.classList.remove('highlighted'));
-  const index = Math.floor(slideGallery.scrollLeft / slideWidth);
-  thumbnailContainer
-    .querySelector(`div[data-id="${index}"]`)
-    .classList.add('highlighted');
-};
-
-const scrollToElement = el => {
-  const index = parseInt(el.dataset.id, 10);
-  slideGallery.scrollTo(index * slideWidth, 0);
-};
-
-thumbnailContainer.innerHTML += [...slides]
-  .map((slide, i) => `<div data-id="${i}"></div>`)
-  .join('');
-
-thumbnailContainer.querySelectorAll('div').forEach(el => {
-  el.addEventListener('click', () => scrollToElement(el));
-});
-
-slideGallery.addEventListener('scroll', e => highlightThumbnail());
-
-highlightThumbnail();
