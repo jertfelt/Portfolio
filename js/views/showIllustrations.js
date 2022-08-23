f4a1283f60428b82f6b988161495c5fed6c74358
Tova Jertfelt <tovizitan@gmail.com>
2022-08-23 10:48:27 +0200

    uppdragsillustrationer uppe - saknas bara att länka till enskilda projekt

--- js/views/showIllustrations.js
+++ js/views/showIllustrations.js
@@ -67,7 +67,6 @@ const drawGallery = (data) => {
 async function getIllustrations() {
   try{
   let res = await fetch("../js/data/ownIllustrations.json");
-
   const data= await res.json();
 
   illustrationsArray = [...data.ownIllustrations];
@@ -83,4 +82,90 @@ console.log(error)}
 }
 
 getIllustrations();
+
+
+
+
+
+
+const jobillustrationGallery = document.getElementById("galleryJobIllustration");
+const slideDivjobIllustrationGallery = document.getElementById("slidesIllustrations")
+
+let firstCol = document.getElementById("firstCol");
+
+let secondCol = document.getElementById("2ndCol");
+
+let jobillustrationsArray = [];
+
+//Create:
+const drawJobIllustrationGallery = (data) => {
+  // console.log(data);
+  let even = data.filter(function(element, index, array) {
+    return (index %2 ===0)
+  })
+  let odd = data.filter(function(element, index, array) {
+    return (index %2 !==0)
+  })
+
+  even.forEach(item => {
+    let content = document.createElement("div");
+    content.innerHTML=`
+    <div class="pointer"
+    id="${item.fields.title}">
+    <a href="${item.fields.link}">
+    <img src="${item.fields.image.fields.file.url}"
+    alt="${item.fields.title} ${item.fields.description}">
+    <span class="gallery__description">
+    <h3 class="text--m headerfont">${item.fields.title}</h3>
+    <h4 class="text--s">${item.fields.description}</h4>
+    </span>
+    </a>
+    </div>
+    `
+    secondCol.appendChild(content);
+  })
+
+  odd.forEach(item => {
+    let content = document.createElement("div");
+    content.innerHTML=`
+    <div class="pointer"
+    id="${item.fields.title}">
+    <a href="${item.fields.link}">
+    <img src="${item.fields.image.fields.file.url}"
+    alt="${item.fields.title} ${item.fields.description}">
+    <span class="gallery__description">
+    <h3 class="text--m headerfont">${item.fields.title}</h3>
+    <h4 class="text--s">${item.fields.description}</h4>
+    </span>
+    </a>
+    </div>
+    `
+    firstCol.appendChild(content);
+  })
+
+ 
+  
+}
+
+
+
+async function getJobIllustrations() {
+  try{
+  let res = await fetch("../js/data/uppdragIllustrations.json");
+
+  const data= await res.json();
+
+  jobillustrationsArray = [...data.uppdragIllustrations];
+
+  const title =  jobillustrationsArray.map(item => {
+    return item;
+  })
+ 
+  drawJobIllustrationGallery(title);
+}
+catch(error){console.log("Fetch fungerar ej, se över getJobIllustrations")
+console.log(error)}
+}
+
+getJobIllustrations();
 
